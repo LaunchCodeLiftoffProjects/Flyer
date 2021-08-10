b1235f7ce7e0324c74eab275c1079d0d7ab2357d
bstoverink <bstoverink@gmail.com>
2021-08-09 20:53:35 -0500

    copied from previous project

---
+++ src/main/java/org/launchcode/demo/controllers/SearchController.java
@@ -0,0 +1,12 @@
+package org.launchcode.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "search")
+public class SearchController {
+
+
+}
