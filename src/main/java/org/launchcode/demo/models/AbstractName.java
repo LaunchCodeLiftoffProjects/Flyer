b8c1057a10e3b71dbfd34cffe025feff47a1df9f
Lakshmi <lakshmi.swathi24@gmail.com>
2021-08-16 19:47:49 -0500

    changes for homepages

--- src/main/java/org/launchcode/demo/models/AbstractName.java
+++ src/main/java/org/launchcode/demo/models/AbstractName.java
@@ -1,2 +1,51 @@
-package org.launchcode.demo.models;public class AbstractName {
+package org.launchcode.demo.models;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class AbstractName {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotBlank(message = "Name is Required")
+    @Size(min=1, max = 250)
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractName that = (AbstractName) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
 }
