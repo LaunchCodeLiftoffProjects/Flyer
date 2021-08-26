6dba027eb93047a7a670dad091439e319b63ec65
bstoverink <bstoverink@gmail.com>
2021-08-25 19:39:34 -0500

    Concert Data class and link bug fix

--- src/main/java/org/launchcode/demo/controllers/ConcertController.java
+++ src/main/java/org/launchcode/demo/controllers/ConcertController.java
@@ -1,25 +1,51 @@
 package org.launchcode.demo.controllers;
 
+import org.launchcode.demo.models.Concert;
+import org.launchcode.demo.models.data.ConcertRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("add")
+@RequestMapping(value = "add")
 public class ConcertController {
 
-    @RequestMapping("")
-    public String add(Model model) {
+    @Autowired
+    private ConcertRepository concertRepository;
+
+    @GetMapping("")
+    public String displayAddConcertForm(Model model) {
+        model.addAttribute("title", "Add Concert");
+        model.addAttribute(new Concert());
+        model.addAttribute("concerts", concertRepository.findAll());
 
         return "add";
     }
 
+    @PostMapping("add")
+    public String processAddConcertForm(@ModelAttribute @Valid Concert newConcert,
+                                        Errors errors, Model model) {
+
+        if(errors.hasErrors()) {
+            return "add";
+        }
+
+        concertRepository.save(newConcert);
+
+        return "redirect:";
+    }
+
 //    private static List<String> add = new ArrayList<>();
-//
+
 //    @GetMapping
 //    public String displayAllConcerts(Model model) {
 //        model.addAttribute("title", "All Concerts");
--- src/main/java/org/launchcode/demo/controllers/ListController.java
+++ src/main/java/org/launchcode/demo/controllers/ListController.java
@@ -1,8 +1,16 @@
 package org.launchcode.demo.controllers;
 
+import org.launchcode.demo.models.Concert;
+import org.launchcode.demo.models.ConcertData;
+import org.launchcode.demo.models.data.ConcertRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
 
 @Controller
 @RequestMapping(value = "list")
@@ -11,8 +19,28 @@ public class ListController {
     @RequestMapping("")
     public String list(Model model) {
 
-
+        model.addAttribute("columns", columnChoices);
 
         return"list";
     }
-}
+
+    @Autowired
+    private ConcertRepository concertRepository;
+
+    static HashMap<String, String> columnChoices = new HashMap<>();
+
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        Iterable<Concert> concerts;
+        if (searchTerm.toLowerCase().equals("all") || searchTerm.equals("")) {
+            concerts = concertRepository.findAll();
+        } else {
+            concerts = ConcertData.findByColumnAndValue(searchType, searchTerm, concertRepository.findAll());
+        }
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("title", "Concerts with " + columnChoices.get(searchType) + ": " + searchTerm);
+        model.addAttribute("concerts", concerts);
+
+            return "list";
+        }
+    }
